5aa2d8efa1f4efa75f95ce924e1893a25a4ba6e8
Ryan LeFevre <meltingice8917@gmail.com>
2018-03-24 10:24:00 -0400

    Swap logo to Nano logo

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ var proxy = require('http-proxy-middleware');
 
 var app = express();
 
-app.use('/', proxy({ target: 'http://10.0.1.52:7076', changeOrigin: true }));
+app.use('/', proxy({ target: 'http://10.0.1.152:7076', changeOrigin: true }));
 app.listen(3001);
--- src/app/Navigation.js
+++ src/app/Navigation.js
@@ -1,19 +1,20 @@
 import React from 'react'
+import ReactSVG from 'react-svg'
 import { NavLink } from 'react-router-dom'
 
 import './Navigation.css'
-import Logo from './images/logo.png'
+import Logo from './images/logo.svg'
 
 export default () => {
   return (
     <div id="Navigation" className="col p-4 h-100">
       <div className="LogoWrap">
-        <img src={Logo} /> RaiBlocks Control Panel
+        <ReactSVG path={Logo} className="Logo" /> Control Panel
       </div>
 
       <ul className="NavigationList list-group list-group-flush mt-4">
         <li className="list-group-item">
-          <NavLink exact to="/" activeClassName="active">Synchronization</NavLink>
+          <NavLink exact to="/" activeClassName="active">Node Status</NavLink>
         </li>
         <li className="list-group-item">
           <NavLink exact to="/delegators" activeClassName="active">Delegators</NavLink>
