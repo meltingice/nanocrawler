85a1ef70a66fb67d7f0cf2d61d5b8dceeb80346e
Ryan LeFevre <meltingice8917@gmail.com>
2018-04-20 16:56:39 -0400

    Add send block to explorer

--- src/app/partials/explorer/block/SendBlock.js
+++ src/app/partials/explorer/block/SendBlock.js
@@ -1,3 +1,73 @@
 import React from "react";
+import accounting from "accounting";
+import AccountLink from "../../AccountLink";
+import BlockLink from "../../BlockLink";
+import PriceWithConversions from "../../PriceWithConversions";
 
-export default function SendBlock() {}
+import "./Block.css";
+
+export default function SendBlock({ block }) {
+  return (
+    <div className="Block">
+      <h4 className="mb-0">
+        Sender{" "}
+        <small>
+          <AccountLink
+            account={block.block_account}
+            className="text-muted break-word"
+          />
+        </small>
+      </h4>
+      <p>
+        <small>The account that initiated the transaction</small>
+      </p>
+
+      <h4 className="mb-0">
+        Recipient{" "}
+        <small>
+          <AccountLink
+            account={block.contents.destination}
+            className="text-muted break-word"
+          />
+        </small>
+      </h4>
+      <p>
+        <small>The account that is receiving the transaction</small>
+      </p>
+
+      <h4 className="mb-0">
+        Amount{" "}
+        <small className="text-muted">
+          <PriceWithConversions amount={block.amount} usd btc />
+        </small>
+      </h4>
+      <p>
+        <small>The amount of NANO that was sent in this transaction</small>
+      </p>
+
+      <h5 className="mb-0">
+        Previous Block{" "}
+        <small>
+          <BlockLink
+            hash={block.contents.previous}
+            className="text-muted break-word"
+          />
+        </small>
+      </h5>
+      <p>
+        <small>The previous block in this account's chain</small>
+      </p>
+
+      <h5>
+        Proof of Work{" "}
+        <small className="text-muted break-word">{block.contents.work}</small>
+      </h5>
+      <h5>
+        Signature{" "}
+        <small className="text-muted break-word">
+          {block.contents.signature}
+        </small>
+      </h5>
+    </div>
+  );
+}
