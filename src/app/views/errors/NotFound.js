7ecaafa4b7b15db602f69c1025c22ac93c4e6370
Ryan LeFevre <meltingice8917@gmail.com>
2018-04-23 09:50:27 -0400

    Fix 404 page on mobile

--- src/app/views/errors/NotFound.js
+++ src/app/views/errors/NotFound.js
@@ -3,11 +3,11 @@ import carlos from "./404.gif";
 
 export default function NotFound() {
   return (
-    <div className="row my-5">
+    <div className="row my-5 mx-0">
       <div className="col text-center">
         <h1 className="display-1">404</h1>
-        <h3>This page doesn't seem to exist</h3>
-        <img src={carlos} />
+        <h3 className="mb-4">This page doesn't seem to exist</h3>
+        <img src={carlos} className="w-100" />
       </div>
     </div>
   );
