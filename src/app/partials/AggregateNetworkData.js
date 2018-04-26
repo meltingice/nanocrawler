41a11c8b64ec8fbea6d5da333faa88b7da0d6def
Ryan LeFevre <meltingice8917@gmail.com>
2018-04-26 16:36:30 -0400

    Add dedicated section for known NANO services

--- src/app/partials/AggregateNetworkData.js
+++ src/app/partials/AggregateNetworkData.js
@@ -65,6 +65,13 @@ class AggregateNetworkData extends React.Component {
     );
   }
 
+  servicePeers() {
+    const { config } = this.props;
+    const { peers } = this.state;
+
+    return peers.filter(peer => config.serviceMonitors.includes(peer.url));
+  }
+
   render() {
     if (this.state.peers.length === 0) return null;
     const blockStats = this.blockStats();
@@ -154,6 +161,26 @@ class AggregateNetworkData extends React.Component {
             <h3>{accounting.formatNumber(blockStats.uncheckedBlocks.max)}</h3>
           </div>
         </div>
+
+        <div className="row mt-5 align-items-center">
+          <div className="col-sm">
+            <h3 className="mb-0">NANO Services</h3>
+            <p className="text-muted mb-0">
+              The current status of known NANO services. Wallets, tip bots,
+              merchant tools, etc.
+            </p>
+            <p>
+              <small>
+                Synced within:{" "}
+                <span className="text-success">1,000 blocks</span>{" "}
+                <span className="text-warning">10,000 blocks</span>{" "}
+                <span className="text-danger">greater than 10,000 blocks</span>
+              </small>
+            </p>
+          </div>
+        </div>
+
+        <DiscoveredPeers peers={this.servicePeers()} stats={blockStats} />
 
         <div className="row mt-5 align-items-center">
           <div className="col-sm">
