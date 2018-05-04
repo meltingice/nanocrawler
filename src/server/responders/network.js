ba8a958438e4fcdb4a2c64a04689dd7a7b3efd85
Ryan LeFevre <meltingice8917@gmail.com>
2018-05-04 15:50:48 -0400

    Pull representatives for rich list and change API format

--- src/server/responders/network.js
+++ src/server/responders/network.js
@@ -17,11 +17,33 @@ export default function(app, nano) {
 
   app.get("/rich_list", async (req, res) => {
     try {
-      const data = await redisFetch("richList", 10, async () => {
-        return [];
-      });
+      const richList = await redisFetch(
+        "richListWithAccountData",
+        3600,
+        async () => {
+          const data = await redisFetch("richList", 10, async () => {
+            return [];
+          });
 
-      res.json({ richList: data });
+          const hydratedData = await Promise.all(
+            _.map(data, (balance, account) => {
+              return new Promise((resolve, reject) => {
+                nano.rpc("account_representative", { account }).then(resp => {
+                  resolve({
+                    account,
+                    balance,
+                    representative: resp.representative
+                  });
+                });
+              });
+            })
+          );
+
+          return hydratedData;
+        }
+      );
+
+      res.json({ richList });
     } catch (e) {
       res.status(500).send({ error: e.message });
     }
