4aaf3855cb6791c1193c785624af5b743eb4f4fd
Ryan LeFevre <meltingice8917@gmail.com>
2018-06-02 16:21:08 -0400

    Another type change

--- src/app/partials/network/StateBlock.js
+++ src/app/partials/network/StateBlock.js
@@ -5,7 +5,7 @@ export default function StateBlock({ count }) {
   return (
     <h3>
       {accounting.formatNumber(count)}{" "}
-      <small className="text-muted">state blocks generated</small>
+      <small className="text-muted">state blocks</small>
     </h3>
   );
 }
