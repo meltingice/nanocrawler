2798718f67ed162297a07aaa0ad0554d478a387f
Ryan LeFevre <meltingice8917@gmail.com>
2018-08-31 14:51:46 -0400

    Split recurring jobs into their own apps so API can be scaled

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,38 @@
 module.exports = {
   apps: [
     {
-      name: "nano-server",
-      script: "node server.js",
+      name: "explorer-api",
+      script: "server.api.js",
+      env: {
+        NODE_ENV: "development"
+      },
+      env_production: {
+        NODE_ENV: "production"
+      }
+    },
+    {
+      name: "explorer-peers",
+      script: "server.peers.js",
+      env: {
+        NODE_ENV: "development"
+      },
+      env_production: {
+        NODE_ENV: "production"
+      }
+    },
+    {
+      name: "explorer-top-accounts",
+      script: "server.top-accounts.js",
+      env: {
+        NODE_ENV: "development"
+      },
+      env_production: {
+        NODE_ENV: "production"
+      }
+    },
+    {
+      name: "explorer-tps",
+      script: "server.tps.js",
       env: {
         NODE_ENV: "development"
       },
