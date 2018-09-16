4327b465b1f5dc55259ff720eb5d854666f7f2fd
Ryan LeFevre <meltingice8917@gmail.com>
2018-09-15 21:59:01 -0400

    Translations for random verified accounts

--- src/app/partials/explorer/RandomVerifiedAccounts.js
+++ src/app/partials/explorer/RandomVerifiedAccounts.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from "react";
+import { FormattedMessage } from "react-intl";
 import _ from "lodash";
 
 import AccountLink from "../AccountLink";
@@ -18,12 +19,24 @@ export default class RandomVerifiedAccounts extends React.Component {
   render() {
     return (
       <Fragment>
-        <h3 className="mb-0">Verified Accounts</h3>
+        <h3 className="mb-0">
+          <FormattedMessage id="ninja.verified_accounts" />
+        </h3>
         <p className="text-muted">
-          A random sampling of accounts verified with{" "}
-          <a href="https://mynano.ninja" target="_blank" className="text-muted">
-            My Nano Ninja
-          </a>
+          <FormattedMessage
+            id="ninja.verified_accounts.desc"
+            values={{
+              link: (
+                <a
+                  href="https://mynano.ninja"
+                  target="_blank"
+                  className="text-muted"
+                >
+                  My Nano Ninja
+                </a>
+              )
+            }}
+          />
         </p>
 
         <hr />
