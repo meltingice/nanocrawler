609ab8f8ecc5b6a45246abae1fe03ebab35e08ef
Ryan LeFevre <meltingice8917@gmail.com>
2018-09-17 16:27:50 -0400

    Add missing Peer Versions string

--- src/translations.js
+++ src/translations.js
@@ -1,12 +1,14 @@
 import en from "./translations/en.json"; // English
 import es from "./translations/es.json"; // Spanish
 import de from "./translations/de.json"; // German
+import id from "./translations/id.json"; // Indonesian
 import ko from "./translations/ko.json"; // Korean
 import zh from "./translations/zh.json"; // Chinese
 
 import localeEn from "react-intl/locale-data/en";
 import localeEs from "react-intl/locale-data/es";
 import localeDe from "react-intl/locale-data/de";
+import localeId from "react-intl/locale-data/id";
 import localeKo from "react-intl/locale-data/ko";
 import localeZh from "react-intl/locale-data/zh";
 
@@ -14,6 +16,7 @@ export const messages = {
   en,
   es,
   de,
+  id,
   ko,
   zh
 };
@@ -22,6 +25,7 @@ export const localeData = [
   ...localeEn,
   ...localeEs,
   ...localeDe,
+  ...localeId,
   ...localeKo,
   ...localeZh
 ];
