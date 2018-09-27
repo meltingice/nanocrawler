20bd40c21e025138be5e6ff75ac7bee0f45818e9
Ryan LeFevre <meltingice8917@gmail.com>
2018-09-27 18:41:10 -0400

    Add renamed files

---
+++ src/app/views/explorer/Accounts.js
@@ -0,0 +1,65 @@
+import React, { Fragment } from "react";
+import { Helmet } from "react-helmet";
+import { FormattedNumber } from "react-intl";
+import injectClient from "lib/ClientComponent";
+
+import AccountLink from "../../partials/AccountLink";
+import PriceWithConversions from "../../partials/PriceWithConversions";
+import DistributionGraph from "../../partials/explorer/frontiers/DistributionGraph";
+import DistributionStats from "../../partials/explorer/frontiers/DistributionStats";
+
+class Accounts extends React.PureComponent {
+  state = {
+    accounts: [],
+    distribution: null,
+    page: 1
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      page: 1,
+      totalAccounts: 0,
+      accounts: [],
+      distribution: {}
+    };
+  }
+
+  async componentDidMount() {
+    const { total, accounts } = this.loadAccounts();
+    const distribution = await this.props.client.wealthDistribution();
+
+    this.setState({
+      totalAccounts: total,
+      accounts,
+      distribution
+    });
+  }
+
+  async loadAccounts() {
+    return await this.props.client.frontierList(this.state.page);
+  }
+
+  render() {
+    return (
+      <div id="FrontierList" className="p-4">
+        <Helmet>
+          <title>All Accounts</title>
+        </Helmet>
+
+        <div className="row">
+          <div className="col">
+            <h1>NANO Distribution</h1>
+            <hr />
+
+            <DistributionGraph distribution={this.state.distribution} />
+            <DistributionStats distribution={this.state.distribution} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default injectClient(Accounts);
