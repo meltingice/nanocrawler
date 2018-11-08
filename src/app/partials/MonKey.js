2de79b27b64fb8249741356207572e8c34231f4c
Ryan LeFevre <meltingice8917@gmail.com>
2018-11-07 23:43:05 -0500

    Implement loading state for MonKeys

--- src/app/partials/MonKey.js
+++ src/app/partials/MonKey.js
@@ -1,10 +1,45 @@
 import React from "react";
+import _ from "lodash";
+import ReactSVG from "react-svg";
+import LoadingMonKey from "app/images/loading-monkey.svg";
 
-export default React.memo(function MonKey({ account, ...props }) {
-  return (
-    <img
-      {...props}
-      src={`https://bananomonkeys.herokuapp.com/image?address=${account}`}
-    />
-  );
-});
+import "./MonKey.css";
+
+export default class MonKey extends React.PureComponent {
+  state = {
+    loaded: false
+  };
+
+  componentDidMount() {
+    const img = document.createElement("img");
+    img.onload = () => {
+      this.setState({ loaded: true });
+    };
+
+    img.src = this.imageUrl;
+  }
+
+  get imageUrl() {
+    const { account } = this.props;
+    return `https://bananomonkeys.herokuapp.com/image?address=${account}`;
+  }
+
+  loadingState() {
+    const { account, ...otherProps } = this.props;
+
+    return (
+      <ReactSVG
+        path={LoadingMonKey}
+        className="LoadingMonKey"
+        {...otherProps}
+      />
+    );
+  }
+
+  render() {
+    const { account, ...otherProps } = this.props;
+    if (!this.state.loaded) return this.loadingState();
+
+    return <img {...otherProps} src={this.imageUrl} />;
+  }
+}
