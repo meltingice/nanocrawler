666afe0d4233a549b5d4eda6d43655355f2651ce
Ryan LeFevre <meltingice8917@gmail.com>
2018-11-19 15:32:29 -0500

    Cache supply calculation for 5 minutes

--- src/server/responders/v2/network.js
+++ src/server/responders/v2/network.js
@@ -1,3 +1,4 @@
+import redisFetch from "../../helpers/redisFetch";
 import Currency from "../../../lib/Currency";
 import clientConfig from "../../../client-config.json";
 
@@ -6,30 +7,34 @@ const MAX_SUPPLY_RAW = "340282366920938463463374607431768211455";
 export default function(app, nano) {
   app.get("/network/supply", async (req, res) => {
     try {
-      const balances = (await nano.accounts.balances([
-        clientConfig.currency.genesisAccount,
-        clientConfig.currency.emissionAccount
-      ])).balances;
+      const data = await redisFetch("v2/network/supply", 300, async () => {
+        const balances = (await nano.accounts.balances([
+          clientConfig.currency.genesisAccount,
+          clientConfig.currency.emissionAccount
+        ])).balances;
 
-      const genesisBalance =
-        balances[clientConfig.currency.genesisAccount].balance;
-      const emissionBalance =
-        balances[clientConfig.currency.emissionAccount].balance;
+        const genesisBalance =
+          balances[clientConfig.currency.genesisAccount].balance;
+        const emissionBalance =
+          balances[clientConfig.currency.emissionAccount].balance;
 
-      const availableSupply = Currency.subtractRaw(
-        MAX_SUPPLY_RAW,
-        genesisBalance
-      );
+        const availableSupply = Currency.subtractRaw(
+          MAX_SUPPLY_RAW,
+          genesisBalance
+        );
 
-      const circulatingSupply = Currency.subtractRaw(
-        availableSupply,
-        emissionBalance
-      );
+        const circulatingSupply = Currency.subtractRaw(
+          availableSupply,
+          emissionBalance
+        );
 
-      res.json({
-        available: availableSupply,
-        circulating: circulatingSupply
+        return {
+          available: availableSupply,
+          circulating: circulatingSupply
+        };
       });
+
+      res.json(data);
     } catch (e) {
       res.status(500).send({ error: e.message });
     }
