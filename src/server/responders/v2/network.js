08dfe63e4ca04b2b6deaf6094ff4ead0fd23a7b7
Ryan LeFevre <meltingice8917@gmail.com>
2018-11-19 15:30:22 -0500

    Some more supply bug fixes

--- src/server/responders/v2/network.js
+++ src/server/responders/v2/network.js
@@ -6,12 +6,15 @@ const MAX_SUPPLY_RAW = "340282366920938463463374607431768211455";
 export default function(app, nano) {
   app.get("/network/supply", async (req, res) => {
     try {
-      const balances = await nano.rpc("accounts_balances", {
-        accounts: [clientConfig.genesisAccount, clientConfig.emissionAccount]
-      });
+      const balances = (await nano.accounts.balances([
+        clientConfig.currency.genesisAccount,
+        clientConfig.currency.emissionAccount
+      ])).balances;
 
-      const genesisBalance = balances[clientConfig.genesisAccount].balance;
-      const emissionBalance = balances[clientConfig.emissionAccount].balance;
+      const genesisBalance =
+        balances[clientConfig.currency.genesisAccount].balance;
+      const emissionBalance =
+        balances[clientConfig.currency.emissionAccount].balance;
 
       const availableSupply = Currency.subtractRaw(
         MAX_SUPPLY_RAW,
