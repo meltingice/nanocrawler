80991f4e71bba47ff95226e8e3ce38da59a02554
Ryan LeFevre <meltingice8917@gmail.com>
2019-04-01 17:25:51 -0400

    Add burn account to supply calculator and include pending balances

--- src/server/helpers/circulatingSupply.js
+++ src/server/helpers/circulatingSupply.js
@@ -8,14 +8,17 @@ const nano = new Nano({ url: serverConfig.nodeHost });
 
 const UNCIRCULATING_ACCOUNTS = [
   "ban_1fundm3d7zritekc8bdt4oto5ut8begz6jnnt7n3tdxzjq3t46aiuse1h7gj",
-  "ban_3fundbxxzrzfy3k9jbnnq8d44uhu5sug9rkh135bzqncyy9dw91dcrjg67wf"
+  "ban_3fundbxxzrzfy3k9jbnnq8d44uhu5sug9rkh135bzqncyy9dw91dcrjg67wf",
+  "ban_1burnbabyburndiscoinferno111111111111111111111111111aj49sw3w"
 ];
 
 export default async function circulatingSupply() {
   const uncirculating = _.values(
     (await nano.accounts.balances(UNCIRCULATING_ACCOUNTS)).balances
   )
-    .map(a => parseFloat(Currency.fromRaw(a.balance), 10))
+    .map(a =>
+      parseFloat(Currency.fromRaw(Currency.addRaw(a.balance, a.pending)), 10)
+    )
     .reduce((acc, val) => acc + val);
 
   const circulating = clientConfig.currency.maxSupply - uncirculating;
