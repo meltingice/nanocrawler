da36806deb6001c3490a7feb6533eb850db80b13
Ryan LeFevre <meltingice8917@gmail.com>
2019-04-16 11:41:18 -0400

    Move timestamp export script

--- scripts/export-timestamps.js
+++ scripts/export-timestamps.js
@@ -1,13 +1,16 @@
+// This is used to export timestamps from NanoCrawler's own system, which
+// can then be used to import into other explorers or the Nano node itself.
+
 const fs = require("fs");
 const util = require("util");
 const redis = require("redis").createClient();
 const scan = util.promisify(redis.scan).bind(redis);
 const get = util.promisify(redis.get).bind(redis);
 
-const stream = fs.createWriteStream("timestamps.csv");
+const outputFile = process.argv[2] || "timestamps.csv";
+const stream = fs.createWriteStream(outputFile);
 const HASH_REGEX = /^block_timestamp\/([A-F0-9]{64})$/;
 
-let nextCursor;
 async function fetchNext(cursor) {
   console.log(`Fetching cursor ${cursor}`);
   const resp = await scan(
@@ -18,7 +21,7 @@ async function fetchNext(cursor) {
     "5000"
   );
 
-  nextCursor = resp[0];
+  const nextCursor = resp[0];
   const keys = resp[1];
   const done = nextCursor === "0";
 
