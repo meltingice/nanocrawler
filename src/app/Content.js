c225d75b96ae47ba88363394949bca32578fa3aa
Brandon Berhent <bbedward@gmail.com>
2020-06-07 11:17:17 -0400

    Remove extraneous change

--- src/app/Content.js
+++ src/app/Content.js
@@ -16,15 +16,7 @@ const ExplorerAccount = React.lazy(() => import("./views/explorer/Account"));
 const ExplorerBlock = React.lazy(() => import("./views/explorer/Block"));
 const Accounts = React.lazy(() => import("./views/explorer/Accounts"));
 
-class Content extends React.Component {
-  constructor(props) {
-    super(props);
- 
-    this.state = {
-      natriconOn: true
-    };
-  }
-   
+class Content extends React.Component {   
   state = {
     hasError: false
   };
